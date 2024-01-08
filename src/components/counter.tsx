f66a7330c97fe1e9290b7f4ae77ab16d32b038c9
Leyla-Li <lileyla62@gmail.com>
2024-01-07 18:22:27 -0800

    useReducer

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,35 +1,83 @@
-import { useEffect, useState } from 'react';
+import { useReducer } from 'react';
 
-const Counter = (): JSX.Element => {
-  const [count, setCount] = useState(0);
-  const [draftCount, setDraftCount] = useState(count);
+type InitialState = {
+  count: number;
+  draftCount: string | number;
+};
 
-  useEffect(() => {
-    setDraftCount(count);
-  }, [count]);
+type Action = {
+  type: 'increment' | 'decrement' | 'reset' | 'updateCountFromDraft';
+};
+
+type ActionWithPayload = {
+  type: 'updateDraftCount';
+  payload: number;
+};
+
+const initialState: InitialState = {
+  count: 0,
+  draftCount: 0,
+};
+
+const reducer = (state = initialState, action: Action | ActionWithPayload) => {
+  console.log({ action });
+  const { count, draftCount } = state;
+
+  if (action.type === 'increment') {
+    const newCount = count + 1;
+    return { count: newCount, draftCount: newCount };
+  }
+
+  if (action.type === 'decrement') {
+    const newCount = count - 1;
+    return { count: newCount, draftCount: newCount };
+  }
+
+  if (action.type === 'reset') {
+    return { count: 0, draftCount: 0 };
+  }
+
+  if (action.type === 'updateDraftCount') {
+    console.log('updateDraftCount');
+    return { count, draftCount: action.payload };
+  }
+
+  if (action.type === 'updateCountFromDraft') {
+    return { count: Number(draftCount), draftCount };
+  }
+
+  return state;
+};
+
+const Counter = () => {
+  const [{ count, draftCount }, dispatch] = useReducer(reducer, initialState);
 
   return (
     <section className="flex w-2/3 flex-col items-center gap-8 border-4 border-primary-500 bg-white p-8 shadow-lg">
       <h1>Days Since the Last Accident</h1>
       <p className="text-6xl">{count}</p>
       <div className="flex gap-2">
-        <button onClick={() => setCount((count) => count - 1)}>
+        <button onClick={() => dispatch({ type: 'decrement' })}>
           ➖ Decrement
         </button>
-        <button onClick={() => setCount(0)}>🔁 Reset</button>
-        <button onClick={() => setCount(count + 1)}>➕ Increment</button>
+        <button onClick={() => dispatch({ type: 'reset' })}>🔁 Reset</button>
+        <button onClick={() => dispatch({ type: 'increment' })}>
+          ➕ Increment
+        </button>
       </div>
       <div>
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            setCount(draftCount);
+            dispatch({ type: 'updateCountFromDraft' });
           }}
         >
           <input
             type="number"
             value={draftCount}
-            onChange={(e) => setDraftCount(e.target.valueAsNumber)}
+            onChange={(e) =>
+              dispatch({ action: 'updateDraftCount', payload: e.target.value })
+            }
           />
           <button type="submit">Update Counter</button>
         </form>
